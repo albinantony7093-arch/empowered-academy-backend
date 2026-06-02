d815f53e23f8744ab4869d55bd2de96ad6a0e238
Aleenajoshy <aleenajoshy02002@gmail.com>
2026-06-02 23:11:01 +0530

    email sneding during enroll fixed

--- app/main.py
+++ app/main.py
@@ -34,7 +34,7 @@ if settings.SENTRY_DSN:
     )
     logger.info("Sentry initialised")
 
-app = FastAPI(title="Empowered Academy API", version="1.3.0", docs_url=None, redoc_url=None, openapi_url=None)
+app = FastAPI(title="Empowered Academy API", version="1.3.0")
 
 
 def _register_models() -> None:
